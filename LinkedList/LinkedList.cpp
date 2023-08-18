34e3c729f5f28f6aef8e1394715585e1aeaff5d8
Siddharth Charkhawala <sid.charkhawala@gmail.com>
2023-08-18 23:42:26 +0530

    DeleteTail function of linkedlist is added.

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -69,6 +69,22 @@ Node* DeleteHead(Node* head){
     }
 }
 
+Node* DeleteTail(Node* head){
+    if(head == NULL) return NULL;
+
+    if(head->next == NULL){
+        delete head;
+        return NULL;
+    }
+
+    Node* curr = head;
+    while(curr->next->next != NULL) curr = curr->next;
+
+    delete(curr->next);
+    curr->next = NULL;
+    return head;
+}
+
 int main(){
     Node* head = new Node(10);
     head->next = new Node(20);
@@ -106,6 +122,12 @@ int main(){
     head = DeleteHead(head);
 
     cout<<"After deleting head: ";
+    PrintList(head);
+    cout<<endl;
+
+    head = DeleteTail(head);
+
+    cout<<"After deleting tail: ";
     PrintList(head);
     cout<<endl;
 
