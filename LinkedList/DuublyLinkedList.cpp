a5b538bed6ef01b7edef39dc69b810b840c3ccae
Siddharth Charkhawala <sid.charkhawala@gmail.com>
2023-08-19 21:47:56 +0530

    DoublyLinkedList file is added.

---
+++ LinkedList/DuublyLinkedList.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+void PrintDoublyLinkedList(Node* head){
+    while(head != NULL){
+        cout<<head->data<<" ";
+        head = head->next;
+    }
+}
+
+int main(){
+
+    Node* head = new Node(10);
+    Node* temp1 = new Node(20);
+    Node* temp2 = new Node(30);
+
+    head->next = temp1;
+    temp1->prev = head;
+    temp1->next = temp2;
+    temp2->prev = temp1;
+
+    PrintDoublyLinkedList(head);
+    return 0;
+}
