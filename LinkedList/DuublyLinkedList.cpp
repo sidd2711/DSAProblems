7cf548e0bcdf2a4a00557fc4365fa464732290bc
Siddharth Charkhawala <sid.charkhawala@gmail.com>
2023-08-20 23:18:02 +0530

    InsertBegin & InsertEnd function is added in doubly LinkedList

--- LinkedList/DuublyLinkedList.cpp
+++ LinkedList/DuublyLinkedList.cpp
@@ -20,6 +20,28 @@ void PrintDoublyLinkedList(Node* head){
     }
 }
 
+Node*InsertBegin(Node* head, int x){
+    Node* temp = new Node(x);
+    
+    if(head != NULL) head->prev = temp;
+    temp->next = head;
+    return temp;
+}
+
+Node* InsertEnd(Node* head, int x){
+
+    Node* temp = new Node(x);
+    if(head == NULL) return temp;
+    Node* curr = head;
+    while(curr->next != NULL) curr = curr->next;
+
+    
+    temp->prev = curr;
+    curr->next = temp;
+
+    return head;
+}
+
 int main(){
 
     Node* head = new Node(10);
@@ -32,5 +54,36 @@ int main(){
     temp2->prev = temp1;
 
     PrintDoublyLinkedList(head);
+    cout<<endl;
+
+    head = InsertBegin(head,5);
+    head = InsertBegin(head,4);
+    head = InsertBegin(head,3);
+
+    cout<<"After InsertBegin: ";
+    PrintDoublyLinkedList(head);
+    cout<<endl;
+
+    Node* temp = NULL;
+    temp = InsertBegin(temp,1);
+
+    cout<<"After InsertBegin in Empty LL: ";
+    PrintDoublyLinkedList(temp);
+    cout<<endl;
+
+    head = InsertEnd(head,40);
+    head = InsertEnd(head,50);
+
+    cout<<"After InsertEnd: ";
+    PrintDoublyLinkedList(head);
+    cout<<endl;
+
+    Node* InsertEndInNull = NULL;
+    InsertEndInNull = InsertEnd(InsertEndInNull,10);
+
+    cout<<"After InsertEnd in Empty LL: ";
+    PrintDoublyLinkedList(InsertEndInNull);
+    cout<<endl;
+
     return 0;
 }
